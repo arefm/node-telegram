9df86bbddda27fe098f5a184d4b3a4e3798fe035
Aref Mirhosseini <code@arefmirhosseini.com>
2017-11-11 14:42:36 +0330

    beta-1

--- bin/server.js
+++ bin/server.js
@@ -7,19 +7,34 @@ import { telegram as configs } from '../configs/configs'
 
 class TelegramClient {
 
-	constructor(ApiToken) {
-		// console.log(process.env.npm_package_config_baseUrl)
-		this.ApiReq = rp.defaults({
-			baseUrl: `${configs.base_url}${ApiToken}`,
-			headers: { 'Content-Type': 'application/json' }
-		})
-		// set hooks
-		this.SetHook(configs.base_hook_url, {
+    constructor(ApiToken) {
+        // console.log(process.env.npm_package_config_baseUrl)
+        this.ApiReq = rp.defaults({
+            baseUrl: `${configs.base_url}${ApiToken}`,
+            headers: { 'Content-Type': 'application/json' }
+        })
+        
+        return {
+            listen: this.ListenMessages.bind(this),
+            reply: this.ReplyMessage.bind(this)
+        }
+    }
+
+    ListenMessages(baseHookUrl) {
+        if (typeof baseHookUrl === 'undefined') {
+            throw new Error('You have to set "listenerUrl" before injecting client listener.').message
+            return
+        }
+        this.SetHook(baseHookUrl, {
             allowed_updates: ['message', 'callback_query']
         })
-	}
+    }
 
-	SetHook(url, options = {}) {
+    SetHook(url, options = {}) {
+        if (typeof url === 'undefined') {
+            throw new Error('You have to set "listenerUrl" before injecting client listener.').message
+            return
+        }
         let opts = Object.assign({}, { url: url }, options)
         this.ApiReq({
                 url: '/setWebhook',
@@ -27,31 +42,36 @@ class TelegramClient {
                 json: true,
                 body: opts
             })
-        	.then(hook => {
-        		console.log(`Setting Webhook: ${hook.result} / ${hook.description}`)
-        	})
+            .then(hook => {
+                console.log(`ListenerUrl: ${hook.result} / ${hook.description}`)
+            })
             .catch(this.ErrorHandler)
     }
 
-    SendMessage(ChatID, text, opts = {}) {
+    ReplyMessage(ChatID, text, opts = {}) {
+        if (typeof ChatID === 'undefined') {
+            throw new Error('You have to set "chat id" before replying.').message
+            return
+        }
         let reqBody = Object.assign({}, {
             chat_id: ChatID,
             text: text
         }, opts)
         this.ApiReq({
-                url: '/sendMessage',
-                method: 'POST',
-                json: true,
-                body: reqBody
-            })
-            .then(resp => {
-                debug('Message Sent: %j', resp)
-            })
-            .catch(this.ErrorHandler)
+            url: '/sendMessage',
+            method: 'POST',
+            json: true,
+            body: reqBody
+        })
+        .then(resp => {
+            debug('Message Sent: %j', resp)
+        })
+        .catch(this.ErrorHandler)
+        return { message: `sending message to chat:${ChatID}...` }
     }
 
     ErrorHandler(err) {
-    	throw new Error(err)
+        throw new Error(err)
     }
 
 }
